db063abb8ca533ce142715dfd497c5ef21b47afc
sunsicheng <393373351@qq.com>
2024-09-15 09:42:42 +0800

    SocketTextStrema案例

---
+++ src/main/java/com/bigdata/flink/quickstart/WorldCount.java
@@ -0,0 +1,36 @@
+package com.bigdata.flink.quickstart;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author: hadoop
+ * @create-date: 2024/9/13 22:06
+ */
+
+public class WorldCount {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> ds = env.socketTextStream("127.0.0.1", 777);
+        ds.flatMap(new FlatMapFunction<String, Tuple2<String,Integer>>() {
+            @Override
+            public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] arr = s.split(" ");
+                for (String string : arr) {
+                    
+                collector.collect(Tuple2.of(string,1));
+                }
+            }
+        }).keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Integer> stringIntegerTuple2) throws Exception {
+                return stringIntegerTuple2.f0;
+            }
+        }).sum(1).print();
+        env.execute();
+    }
+}
